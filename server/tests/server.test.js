cc30b47711f448f790ac9a169480e1280fccba97
Kelly Hair <kelly@routerlab.net>
2017-12-20 10:28:42 -0500

    Troubleshooting server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
+
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
