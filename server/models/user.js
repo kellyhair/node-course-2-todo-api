07737847116bc2086fdd1d6dd4e99754f34cd57d
Kelly Hair <kelly@routerlab.net>
2017-12-21 17:02:53 -0500

    update config to use JSON file

--- server/models/user.js
+++ server/models/user.js
@@ -52,7 +52,7 @@ UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
 
-  token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({access, token});
   return user.save().then(() => {
@@ -79,7 +79,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
 
     return Promise.reject();
